6d664ec67d127bcd29e630776befe4098f8aa827
Thanyani Tshikonwani <thanyani.tshikonwani@standardbank.co.za>
2022-12-03 21:51:12 +0200

    project_structure

---
+++ ClearScore/ClearScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClearScoreApp.swift
+//  ClearScore
+//
+//  Created by Thanyani Tshikonwani on 2022/12/03.
+//
+
+import SwiftUI
+
+@main
+struct ClearScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            DashboardView()
+        }
+    }
+}
